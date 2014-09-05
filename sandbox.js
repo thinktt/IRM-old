8f41f87300b12462d7e782bdca2b7b2ea10186ea
thinktt <github@thinktt.e4ward.com>
2014-09-05 14:08:00 -0500

    house cleaning

--- sandbox.js
+++ sandbox.js
@@ -1,17 +1,136 @@
-//test edit
-var $messageBox = $("#messageBox textarea");
-var $outputBox = $("#outputBox textarea"); 
+var globie = 10; 
+
+function say667() {
+    // Local variable that ends up within closure
+    var num = 666;
+    
+    var sayAlert = function() { 
+    	console.log(num + ' ' + globie);
+    	num++;  
+    	globie++; 
+    }
+    
+    return sayAlert;
+}
 
 
-var logOutput = function(output) {
-	$outputBox.val($outputBox.val() + output +'\n');
-};
+var a = say667();
+var b = say667(); 
+a(); 
+a(); 
+a(); 
+a(); 
+b(); 
+a();
+b(); 
+
+console.log(globie);  
 
 
-$("#mainButton").click(function(event) {
 
-	var incidentEmail = $messageBox.val(); 
-	var incidentArray =[];
+
+
+
+
+
+/*
+var a = 'global variable'; 
+
+
+function foo() {
+    var a = 'private variable';
+    
+    function bar() {
+        console.log(a);
+    }
+
+    return bar; 
+}
+
+
+
+console.log(foo()); 
+
+var callAlert = foo();
+
+console.log(a); 
+callAlert(); 
+
+
+var my2DArray = [];
+
+for (var i = 0; i < 5; i++) {
+	my2DArray.push([1,2]);
+}
+
+console.log(my2DArray[0][1]); 
+
+
+
+
+
+var name = 'Brady';
+var name; 
+console.log(name); 
+
+
+var myAlerts = [];
+
+for (var i = 0; i < 5; i++) {
+    myAlerts.push(
+        function inner() {
+            console.log(i);
+        }
+    );
+}
+
+myAlerts[0](); // 5
+myAlerts[1](); // 5
+myAlerts[2](); // 5
+myAlerts[3](); // 5
+myAlerts[4](); // 5s
+
+
+
+function howdy(greeting) {
+	
+	console.log(inner);
+
+	function inner() {
+		//bobby
+	}
+
+
+	console.log(inner);
+	console.log(typeof inner2);
+	console.log(typeof a);  
+
+	var inner = function() {
+		//brad
+	} 
+
+
+	var inner2 = function() {};
+	var greeting; 
+	var a = 10; 
+
+	console.log(greeting); 
+
+	greeting = 'Howdy Y\'all';
+	console.log(greeting);  
+}
+
+
+
+howdy('Yo!'); 
+console.log(typeof howdy); 
+
+
+//console.log(hello); 
+
+
+/*
+var incidentArray =[];
 	var incident = {}; //incdint obj 
 
 	incidentArray = incidentEmail.split('\n'); 
@@ -42,11 +161,6 @@ $("#mainButton").click(function(event) {
 });
 
 
-
-
-
-
-/*
 $( document ).ready(function() {
 
  

d1b01113f81518045bdfaa3a7212c8cba0fc70a6
thinktt <github@thinktt.e4ward.com>
2014-09-15 10:40:39 -0500

    add delete option to comments

--- incident.js
+++ incident.js
@@ -109,74 +109,74 @@ app.controller('Ctrl', function($scope) {
 	};
 
 	$scope.leaders = [
-	    {name: 'Tobias'},
-	    {name: 'Cynthia'} ,
-	    {name: 'Lauren'} ,
-	    {name: 'Ryan'} ,
-	    {name: 'Jeremiah'},
-	    {name: 'John'},
-	    {name: 'Nne'}
-  	];
+		 {name: 'Tobias'},
+		 {name: 'Cynthia'} ,
+		 {name: 'Lauren'} ,
+		 {name: 'Ryan'} ,
+		 {name: 'Jeremiah'},
+		 {name: 'John'},
+		 {name: 'Nne'}
+	];
 
-  	$scope.incedentTypes = [
-  		'Absent', 
-  		'Tardy', 
-  		'Approachability', 
-  		'Dress Code'
-  	];
+	$scope.incedentTypes = [
+		'Absent', 
+		'Tardy', 
+		'Approachability', 
+		'Dress Code'
+	];
 
-  	$scope.incidentStatusTypes = [
-  		'Pending Review',
-  		'Unexcused',
-  		'Excused'
-  	];
+	$scope.incidentStatusTypes = [
+		'Pending Review',
+		'Unexcused',
+		'Excused'
+	];
 
-  	$scope.labs = [
-  		{
-  			name: 'SCC', 
-  			stations: ['Print Room', 'Info', 'Resource']
-  		},
-  		{
-  			name: 'BLOC', 
-  			stations: ['Print Room','Help Desk', 'ZACH']
-  		},
-  		{
-  			name: 'WCL', 
-  			stations: ['Print Room', 'Help Desk']
-  		},
-  	];
+	$scope.labs = [
+		{
+			name: 'SCC', 
+			stations: ['Print Room', 'Info', 'Resource']
+		},
+		{
+			name: 'BLOC', 
+			stations: ['Print Room','Help Desk', 'ZACH']
+		},
+		{
+			name: 'WCL', 
+			stations: ['Print Room', 'Help Desk']
+		},
+	];
 
-  	 
+	 
 
-  	$scope.registerStationSelecter = function(stationSelecter) {
-  		$scope.stationSelecter = stationSelecter;
-  	};
+	$scope.registerStationSelecter = function(stationSelecter) {
+		$scope.stationSelecter = stationSelecter;
+	};
 
-  	$scope.updateLabStations = function(selectedLab) {
-  		var i; 
-  		for(i =0; i < $scope.labs.length; i++){
-  			if(selectedLab === $scope.labs[i].name ) {
-  				$scope.labStations = $scope.labs[i].stations;
-  			} 
-  		}
-  	};
+	$scope.updateLabStations = function(selectedLab) {
+		var i; 
+		for(i =0; i < $scope.labs.length; i++){
+			if(selectedLab === $scope.labs[i].name ) {
+				$scope.labStations = $scope.labs[i].stations;
+			} 
+		}
+	};
 
-  	$scope.registerMeetingSelecter = function (meetingSelecter) {
-  		$scope.meetingSelecter = meetingSelecter; 
-  	};
+	$scope.registerMeetingSelecter = function (meetingSelecter) {
+		$scope.meetingSelecter = meetingSelecter; 
+	};
 
-  	$scope.clearMeetingSelecter = function() {
-  		$scope.meetingSelecter.$data = ''; 
-  	};
+	$scope.clearMeetingSelecter = function() {
+		$scope.meetingSelecter.$data = ''; 
+	};
 
-  	$scope.setMeetingPending = function() {
-  		if($scope.incident.meetingDate === '') {
-  			$scope.incident.meetingDate = 'Pending';
-  		} 
-  	};
+	$scope.setMeetingPending = function() {
+		if($scope.incident.meetingDate === '') {
+			$scope.incident.meetingDate = 'Pending';
+		} 
+	};
 
-  	$scope.addComment = function() {
-  		var comment = {
+	$scope.addComment = function() {
+		var comment = {
 			by: 'Tobias',
 			date: '2014-09-07',
 			time: '15:00',
@@ -185,24 +185,30 @@ app.controller('Ctrl', function($scope) {
 			new: true 
 		};
 
-  		$scope.incident.comments.push(comment); 
-  	};
+		$scope.incident.comments.push(comment); 
+	};
 
-  	$scope.clearNewStatus = function(index){
-  		$scope.incident.comments[index].new = false;
-  	};
+	$scope.clearNewStatus = function(index){
+		$scope.incident.comments[index].new = false;
+	};
 
-  	//this function is called on cancel. If it finds
-  	//that the comment is a new comment it deletes it
-  	$scope.checkForNew = function(index){
-  		if($scope.incident.comments[index].new) {
-  			if(confirm('Do you want to delete this comment?')) {
-	  			$scope.incident.comments.splice(index,1);
-  			} else {
-  				$scope.incident.comments[index].new = false;
-  			}
-  		}
-  	};
+	//this function is called on cancel. If it finds
+	//that the comment is a new comment it deletes it
+	$scope.checkForNew = function(index){
+		if($scope.incident.comments[index].new) {
+			if(confirm('Do you want to delete this comment?')) {
+				$scope.incident.comments.splice(index,1);
+			} else {
+				$scope.incident.comments[index].new = false;
+			}
+		}
+	};
+
+	$scope.deleteComment = function(index){
+		if(confirm('Do you want to delete this comment?')) {
+			$scope.incident.comments.splice(index,1);
+		} 
+	};
 
 
 });
@@ -212,20 +218,20 @@ app.filter('toStandardTime', function() {
   var toStandardTime;
   
   toStandardTime = function(milTime) {
-  	var out, milTmRegex, hours24, hours, amPm, minutes; 
-  	milTmRegex = /^[0-9]{2}:[0-9]{2}$/; 
-  	
-  	if (milTmRegex.test(milTime)) {
-  		hours24 = parseInt(milTime.slice(0,2));
-  		hours = ((hours24 + 11) % 12) + 1;
-  		amPm = hours24 > 11 ? ' PM' : ' AM';
-  		minutes = milTime.slice(3,5);
-    	
-    	return hours + ':' + minutes + amPm;
+	var out, milTmRegex, hours24, hours, amPm, minutes; 
+	milTmRegex = /^[0-9]{2}:[0-9]{2}$/; 
+	
+	if (milTmRegex.test(milTime)) {
+		hours24 = parseInt(milTime.slice(0,2));
+		hours = ((hours24 + 11) % 12) + 1;
+		amPm = hours24 > 11 ? ' PM' : ' AM';
+		minutes = milTime.slice(3,5);
+		
+		return hours + ':' + minutes + amPm;
 
 	  } else {
-  		return miltime; 
-  	}
+		return miltime; 
+	}
  };
 
  return toStandardTime; 

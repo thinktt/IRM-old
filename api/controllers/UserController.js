2c72eff2912daa38d5bbed0c93e8baa7e18e77bf
thinktt <github@thinktt.e4ward.com>
2015-07-02 20:52:05 -0500

    sign in and out now working and connected to SPA

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -34,6 +34,55 @@ module.exports = {
 			}
 		
 		});
+	},
+
+	signIn: function(req, res) {
+		var username = req.param('username');
+		var password = req.param('password');
+
+		User.findOne({username: username})
+		.then(function(user) {
+
+			if(!user) return res.notFound('user not found');
+
+			require('machinepack-passwords').checkPassword({
+				passwordAttempt: password,
+				encryptedPassword: user.encryptedPassword
+			})
+			.exec({
+				error: function(err) {
+					res.negotiate(err); 
+				},
+
+				success: function() {
+					req.session.me = user.id;
+					res.ok('user validated');
+				},
+				
+				incorrect: function() {
+					res.notFound('bad password'); 
+				}
+			});
+
+		})
+		.catch(function(err) {
+			console.log('catching error');
+			res.negotiate(err); 
+		});
+	},
+
+	signOut: function(req, res) {
+		req.session.destroy(function(err){
+			if(err) return res.negotiate(err); 
+			return res.ok('user signed out');
+		});
+	},
+
+	checkUserStatus: function(req, res) {
+		if (!req.session.me) return res.ok('not signed in'); 
+		return res.ok('signed in');
 	}
+
+	
 };
 
